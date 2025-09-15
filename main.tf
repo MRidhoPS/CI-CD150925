0429d9ca293bc347bb882973cc04a86ce347c252
mridhophs <mridhophs@gmail.com>
2025-09-15 19:15:53 +0700

    implementation compose, nginx with terraform

--- main.tf
+++ main.tf
@@ -9,16 +9,48 @@ terraform{
 
 provider "docker" {}
 
-resource "docker_image" "nextapp" {
+resource "docker_image" "app_latest" {
     name = "mridhophs/my-next-app:latest"
     keep_locally = false
 }
 
-resource "docker_container" "nextapp" {
-    name = "nextapp"
-    image = docker_image.nextapp.image_id
+resource "docker_image" "app_dev"{
+    name = "mridhophs/my-next-app:dev"
+    keep_locally = false
+}
+
+resource "docker_container" "app_latest" {
+    name = "nextjs-latest"
+    image = docker_image.app_latest.image_id
     ports {
         internal = 3000
-        external = 8080
+        external = 3001
+  }
+}
+
+resource "docker_container" "app_dev" {
+    name = "nextjs-dev"
+    image = docker_image.app_dev.image_id
+    ports {
+        internal = 3000
+        external = 3002
+    }
+}
+
+resource "docker_image" "nginx"{
+    name = "nginx:alpine"
+}
+
+resource "docker_container" "nginx"{
+    name = "nginx-proxy"
+    image = docker_image.nginx.image_id
+    ports {
+        internal = 80
+        external = 80
+    }
+    volumes {
+        host_path      = "${path.module}/nginx.conf"
+        container_path = "/etc/nginx/nginx.conf"
+        read_only      = true
     }
 }
